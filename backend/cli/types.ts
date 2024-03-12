b3f902f5ca3b7d43480342eca445842b48355db9
MOORETOKEN <83512857+MOORETOKEN@users.noreply.github.com>
2024-03-11 18:03:38 -0700

    Update types.ts

--- backend/cli/types.ts
+++ backend/cli/types.ts
@@ -1,22 +1,31 @@
 import * as anchor from '@project-serum/anchor';
 import { PublicKey } from '@solana/web3.js';
 
+export const GLOBAL_AUTHORITY_SEED = "global-authority";
+export const VAULT_SEED = "vault-seed";
+
+export const STAKING_PROGRAM_ID = new PublicKey("2RbwYVj8gmYf8TRNukd34fGJgT7X4X4K3t6gLGwJkNQD");
+export const AMMO_TOKEN_MINT = new PublicKey("H3rmqbVz8NTCkGABeue3yc9PgioL2i1RPrQM45itdKMu");
+export const AMMO_TOKEN_DECIMAL = 1_000_000_000; 
+
+export const USER_POOL_SIZE = 5648;     // 8 + 5640
+
 export interface GlobalPool {
-    lotteryNftCount: anchor.BN,    // 8
-    fixedNftCount: anchor.BN,  // 8
+    // 8 + 40
+    superAdmin: PublicKey,          // 32
+    totalStakedCount: anchor.BN,    // 8
 }
 
-export interface StakedNFT {
-    nftAddr: PublicKey,         // 32
-    stakeTime: anchor.BN,   // 8
-    rank: anchor.BN,         // 8
+export interface StakedData {
+    mint: PublicKey,            // 32
+    stakedTime: anchor.BN,      // 8
+    lockTime: anchor.BN,        // 8
+    duration: anchor.BN,    // 8
 }
 
 export interface UserPool {
-    // 8 + 2456
-    owner: PublicKey,            // 32
-    itemCount: anchor.BN,      // 8
-    items: StakedNFT[],   // 48 * 50
-    rewardTime: anchor.BN,                         // 8
-    pendingReward: anchor.BN,                       // 8
+    // 8 + 5640
+    owner: PublicKey,               // 32
+    stakedCount: anchor.BN,         // 8
+    staking: StakedData[],          // 56 * 100
 }
